01d9812129539f4c62f40eeebec44f7c3640ed18
William Blondel <william.blondel78@gmail.com>
2015-12-09 21:13:07 +0100

    Added : setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='France-IOI',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='William Blondel',
+    author_email='',
+    description=''
+)
